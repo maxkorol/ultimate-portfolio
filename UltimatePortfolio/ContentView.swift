aeeba08d33d8f8e5004850d67570a6ac21a3967c
maxkorol <56126696+maxkorol@users.noreply.github.com>
2025-06-24 20:49:57 +0200

    Initial Commit

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Max Korol on 24/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Max Korol on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
