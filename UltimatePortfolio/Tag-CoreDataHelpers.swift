10236fd4e0de05347cd1ca53cc2bdc662430a6a7
maxkorol <56126696+maxkorol@users.noreply.github.com>
2025-06-25 09:35:18 +0200

    Cleaning up Core Data

---
+++ UltimatePortfolio/Tag-CoreDataHelpers.swift
@@ -0,0 +1,45 @@
+//
+//  Tag-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Max Korol on 25/06/2025.
+//
+
+import Foundation
+import CoreData
+
+extension Tag {
+    var tagID: UUID {
+        id ?? UUID()
+    }
+    
+    var tagName: String {
+        name ?? ""
+    }
+    
+    var tagActiveIssues: [Issue] {
+        let result = issues?.allObjects as? [Issue] ?? []
+        return result.filter { $0.completed == false }
+    }
+    
+    static var example: Tag {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+        return tag
+    }
+}
+
+extension Tag: Comparable {
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        let left = lhs.tagName.localizedLowercase
+        let right = rhs.tagName.localizedLowercase
+        if left == right {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        } else {
+            return left < right
+        }
+    }
+}
